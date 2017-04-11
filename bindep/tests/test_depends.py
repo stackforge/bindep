2c0d262958fac31d66cdf20063a35db460ead11f
Andreas Florath <andreas@florath.net>
2017-04-11 20:53:22 +0000

    Refactor: factor out subprocess and error handling stub
    The implementation of 'get_pkg_version()' was a copy&past&change.
    This patch removes the code duplicates, lowers the cyclomatic
    complexity and emphasizes the real functionality of each method.
    
    Change-Id: I74ebd432d8e1ab8b3433f0f55d5dfe3535019911
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- bindep/tests/test_depends.py
+++ bindep/tests/test_depends.py
@@ -646,7 +646,7 @@ class TestEmerge(TestCase):
         platform = Emerge()
 
         def _side_effect_raise(*args, **kwargs):
-            raise subprocess.CalledProcessError(3, [], '')
+            raise subprocess.CalledProcessError(3, [], b'')
 
         mocked_checkoutput = self.useFixture(
             fixtures.MockPatchObject(subprocess, 'check_output')).mock
@@ -661,7 +661,7 @@ class TestEmerge(TestCase):
         platform = Emerge()
 
         def _side_effect_raise(*args, **kwargs):
-            raise subprocess.CalledProcessError(3, [], '')
+            raise subprocess.CalledProcessError(3, [], b'')
 
         mocked_checkoutput = self.useFixture(
             fixtures.MockPatchObject(subprocess, 'check_output')).mock
