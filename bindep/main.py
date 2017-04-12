d8037453080f9c297b2d3db267b2955da9cba13f
Andreas Florath <andreas@florath.net>
2017-04-12 06:21:47 +0000

    Refactor 'main.py'
    The main method was a 'one-function-does-everything' spaghetti code.
    This patch refactors the main function in a small class and introduces
    methods that do only one thing.  The new version can be easier
    maintained and extended.
    
    Change-Id: Ib58ff9e541e47cc0202f0eb49cf0f0e7929d83fa
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- bindep/main.py
+++ bindep/main.py
@@ -26,59 +26,87 @@ logging.basicConfig(
     stream=sys.stdout, level=logging.INFO, format="%(message)s")
 
 
-def main(depends=None):
-    usage = "Usage: %prog [options] [profile]"
-    parser = optparse.OptionParser(
-        usage=usage, version="%%prog %s" % bindep.version)
-    parser.add_option(
-        "-b", "--brief", action="store_true", dest="brief",
-        help="List only missing packages one per line.")
-    parser.add_option(
-        "-f", "--file", action="store", type="string", dest="filename",
-        default="",
-        help="Package list file (default: bindep.txt or "
-             "other-requirements.txt).")
-    parser.add_option(
-        "--profiles", action="store_true",
-        help="List the platform and configuration profiles.")
-    opts, args = parser.parse_args()
-    if depends is None:
-        depends = bindep.depends.get_depends(opts.filename)
-        if not depends:
-            return 1
-    if opts.profiles:
+class BinDepException(Exception):
+    pass
+
+
+class Bindep(object):
+
+    @staticmethod
+    def parse_args():
+        usage = "Usage: %prog [options] [profile]"
+        parser = optparse.OptionParser(
+            usage=usage, version="%%prog %s" % bindep.version)
+        parser.add_option(
+            "-b", "--brief", action="store_true", dest="brief",
+            help="List only missing packages one per line.")
+        parser.add_option(
+            "-f", "--file", action="store", type="string", dest="filename",
+            default="",
+            help="Package list file (default: bindep.txt or "
+            "other-requirements.txt).")
+        parser.add_option(
+            "--profiles", action="store_true",
+            help="List the platform and configuration profiles.")
+        return parser.parse_args()
+
+    def __init__(self, depends=None):
+        self.__opts, self.__args = self.parse_args()
+        if depends:
+            self.__depends = depends
+        else:
+            self.__depends = bindep.depends.get_depends(self.__opts.filename)
+        if not self.__depends:
+            raise BinDepException("Error setting up depends")
+
+    def __log_profiles(self):
         logging.info("Platform profiles:")
-        for profile in depends.platform_profiles():
+        for profile in self.__depends.platform_profiles():
             logging.info("%s", profile)
         logging.info("")
         logging.info("Configuration profiles:")
-        for profile in depends.profiles():
+        for profile in self.__depends.profiles():
             logging.info("%s", profile)
-    else:
-        if args:
-            profiles = args
-        else:
-            profiles = ["default"]
-        profiles = profiles + depends.platform_profiles()
-        rules = depends.active_rules(profiles)
-        errors = depends.check_rules(rules)
+        return 0
+
+    def __log_errors(self, errors):
         for error in errors:
             if error[0] == 'missing':
-                if opts.brief:
+                if self.__opts.brief:
                     logging.info("%s", "\n".join(error[1]))
                 else:
                     logging.info("Missing packages:")
                     logging.info("    %s", " ".join(error[1]))
             if error[0] == 'badversion':
-                if not opts.brief:
+                if not self.__opts.brief:
                     logging.info("Bad versions of installed packages:")
                     for pkg, constraint, version in error[1]:
                         logging.info(
                             "    %s version %s does not match %s",
                             pkg, version, constraint)
+
+    def __log_missing(self):
+        profiles = self.__args if self.__args else ['default']
+        profiles = profiles + self.__depends.platform_profiles()
+        rules = self.__depends.active_rules(profiles)
+        errors = self.__depends.check_rules(rules)
         if errors:
+            self.__log_errors(errors)
             return 1
-    return 0
+        return 0
+
+    def run(self):
+        if self.__opts.profiles:
+            return self.__log_profiles()
+        return self.__log_missing()
+
+
+def main(depends=None):
+    try:
+        bindep = Bindep(depends)
+        return bindep.run()
+    except Exception:
+        return 1
 
 
 if __name__ == '__main__':
